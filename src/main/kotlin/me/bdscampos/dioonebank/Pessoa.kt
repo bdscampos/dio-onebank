d3dfa48c8258a97c795b525cadc39b4237a3a568
Bruno Campos <bdscampos@gmail.com>
2022-06-03 19:51:11 -0300

    Criação de função e construtor

--- src/main/kotlin/me/bdscampos/dioonebank/Pessoa.kt
+++ src/main/kotlin/me/bdscampos/dioonebank/Pessoa.kt
@@ -5,11 +5,14 @@ class Pessoa {
 
     var cpf: String = "012.345.678-90"
     private set
+
+    constructor()
+
+    fun infoPessoa() = "${nome} e ${cpf}"
 }
 
 fun main() {
     val bruno = Pessoa()
 
-    println(bruno.nome)
-    println(bruno.cpf)
+    println(bruno.infoPessoa())
 }
