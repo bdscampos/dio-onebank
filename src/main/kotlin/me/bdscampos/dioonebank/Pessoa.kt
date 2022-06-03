fd3b7ff31e8c32130a190f804f58d60e73da6832
Bruno Campos <bdscampos@gmail.com>
2022-06-03 19:35:08 -0300

    Configurações iniciais classe pessoa

--- src/main/kotlin/me/bdscampos/dioonebank/Pessoa.kt
+++ src/main/kotlin/me/bdscampos/dioonebank/Pessoa.kt
@@ -1,4 +1,13 @@
 package me.bdscampos.dioonebank
 
 class Pessoa {
+    var nome: String = "Bruno"
+    var cpf: String = "012.345.678-90"
+}
+
+fun main() {
+    val bruno = Pessoa()
+
+    println(bruno.nome)
+    println(bruno.cpf)
 }
